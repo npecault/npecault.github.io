5bf9ff74174f59f5018bcdfd9043f73fd32bb2ef
Noé <noepec1@gmail.com>
2024-06-03 21:14:23 +0200

    feat(ex3): second autofilter for albums

--- src/exercise-three/UserSelection.tsx
+++ src/exercise-three/UserSelection.tsx
@@ -1,16 +1,22 @@
 import AutoFilterInput from "../autocomplete/AutoFilterInput";
 import React, {useState} from "react";
-import {User, users} from "./usersData";
+import {albums, users} from "./usersData";
 
 const UserSelection = () => {
-    const [selectedUser, setSelectedUser] = useState<User | null>(null);
+    const [selectedUser, setSelectedUser] = useState<{ name: string } | null>(null);
+    const [selectedAlbum, setSelectedAlbum] = useState<{ title: string } | null>(null);
 
     return <div className='flex flex-col gap-4 min-h-fit w-fit'>
         <h1>Exercise 3</h1>
-        {selectedUser ? <span><b>Selected:</b> {selectedUser.name}</span> : <b>No user selected, start typing in the search input to see options</b>}
-        <div className='flex'>
+        {selectedUser ? <span><b>Selected:</b> {selectedUser.name}</span> :
+            <b>No user selected, start typing in the search input to see options</b>}
+        {selectedAlbum ? <span><b>Selected album:</b> {selectedAlbum.title}</span> : <b>No album selected</b>}
+
+        <div className='flex flex-col'>
             <label>Search user: </label>
             <AutoFilterInput items={users} lens={x => x.name} valueChange={(user) => setSelectedUser(user)}/>
+            <label>Search album: </label>
+            <AutoFilterInput items={albums} lens={x => x.title} valueChange={(album) => setSelectedAlbum(album)}/>
         </div>
     </div>
 };
