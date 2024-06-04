3f9eae2000ba98a59cea09b1191fb219d9e16d3f
Noé Pécault <noe.pecault@soprasteria.com>
2024-06-04 09:29:23 +0200

    feat(ex2): Added close button for demo

--- src/exercise-two/SpawnModal.tsx
+++ src/exercise-two/SpawnModal.tsx
@@ -1,7 +1,10 @@
 import UserRow from "./UserRow";
 import {DialogType} from "../dialog/DialogType";
+import {useContext} from "react";
+import DialogContext from "../dialog/DialogContext";
 
 const SpawnModal = () => {
+    const {close} = useContext(DialogContext);
     return (
         <div className='flex flex-col gap-4 min-h-fit w-fit'>
             <h1>Exercise 2</h1>
@@ -11,6 +14,7 @@ const SpawnModal = () => {
             <UserRow name='Cookies Lechat' type={DialogType.MODAL}/>
             <h2>Dialog Users</h2>
             <UserRow name='Maurice Ladialog'/>
+            <button className='border-2 rounded-xl p-2 font-bold' onClick={close}>Close current dialog</button>
         </div>
     );
 }
