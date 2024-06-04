02de0389f6db287a2ef879c4a7fd04c62a0425f1
Noé Pécault <noe.pecault@soprasteria.com>
2024-06-04 09:29:08 +0200

    fix(ex2): Removed useless div

--- src/exercise-two/UserRow.tsx
+++ src/exercise-two/UserRow.tsx
@@ -13,10 +13,10 @@ const UserRow = ({name, type = DialogType.DIALOG}: UserRowProps) => {
     return <button onClick={() => open({
         header: <h1>{name}</h1>,
         body: <p>Here are some information about {name}</p>,
+        footer: <i>I'm a footer look at me</i>,
         type: type
     })}>
         <div className='flex flex-row gap-2 border-2 rounded-xl p-2 cursor-pointer'>
-            <div className='rounded-full bg-yellow-500 w-3 h-full'/>
             {name}
         </div>
     </button>;
